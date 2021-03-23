1d90ed2a14550bf9458f6ccf9fb696a6ae0aa26c
Elizabeth Guerra <ecguerra@outlook.com>
2021-03-23 00:17:31 -0400

    styled above fold

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,42 @@
 import { Link } from 'react-router-dom'
 
+import '../home.scss'
+
 const Home = () => {
     return(
         <div className='home'>
             <div className='landing-container'>
-                <h1>Welcome to Blerg!</h1>
-                <p>Hate blogging? Us too. Come on in and join us!</p>
-                <Link to='#'><div className='btn-register'>Start your Blerg!</div></Link>
-
+                <div className='landing-text'>
+                    <h1>Welcome to Blerg!</h1>
+                    <p>Hate blogging? Us too. Come on in and join us</p>
+                    <div className='btn-container'>
+                        <Link to='#' className='btn-register'>Start your Blerg!</Link>
+                    </div>
+                </div>
+                <div className='landing-img'></div>
             </div>
             <div className='article-container'>
-                <h2>What is a Blerg!?</h2>
-                <p>Blerg! is the new blogging platform for people for whom blogging has just never "stuck."
-                Maybe you've tried to blog with good intention, but then abandon your site after a few short weeks.
-                For some of us, blogging just doesn't come naturally...
-                </p>
-                <Link to='#'>Keep Reading</Link>
-            </div>
-            <div className='article-container'>
-                <h2>Blerging Tips</h2>
-                <p>We get it, sometimes you just don't feel like blogging. What should you talk about, how frequently should you post, is anyone reading this?
-                While we can't write your Blerg! for you, we can help you break through that self-doubt and writer's block with these tips...
-                </p>
-                <Link to='#'>Keep Reading</Link>
-            </div>
-            <div className='article-container'>
-                <h2>Blergspiration</h2>
-                <p>Our weekly roundup of Blerging prompts, images, and ideas 
-                </p>
-                <Link to='#'>Keep Reading</Link>
+                <div className='article'>
+                    <h2>What is a Blerg!?</h2>
+                    <p>Blerg! is the new blogging platform for people for whom blogging has just never "stuck."
+                    Maybe you've tried to blog with good intention, but then abandon your site after a few short weeks.
+                    For some of us, blogging just doesn't come naturally...
+                    </p>
+                    <Link to='#'>Keep Reading</Link>
+                </div>
+                <div className='article'>
+                    <h2>Blerging Tips</h2>
+                    <p>We get it, sometimes you just don't feel like blogging. What should you talk about, how frequently should you post, is anyone reading this?
+                    While we can't write your Blerg! for you, we can help you break through that self-doubt and writer's block with these tips...
+                    </p>
+                    <Link to='#'>Keep Reading</Link>
+                </div>
+                <div className='article'>
+                    <h2>Blergspiration</h2>
+                    <p>Our weekly roundup of Blerging prompts, images, and ideas 
+                    </p>
+                    <Link to='#'>Keep Reading</Link>
+                </div>
             </div>
         </div>
     )
